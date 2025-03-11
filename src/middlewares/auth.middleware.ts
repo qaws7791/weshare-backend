3667bd8f2aaf6617f511fa677c210584f96a66a0
qaws7791 <qaws7791@gmail.com>
2025-03-12 01:53:29 +0900

    인증 미들웨어 추가: 인증된 사용자만 핸들러에 접근 가능하도록 구현

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,24 @@
+import { createMiddleware } from "hono/factory";
+import { Context } from "../types/hono";
+
+/**
+ * 인증된 사용자만 핸들러에 접근할 수 있도록 하는 미들웨어
+ * @description c.get("user")가 존재하지 않으면 401 에러를 반환합니다.
+ */
+export const isAuthenticated = createMiddleware<Context>(async (c, next) => {
+  const user = c.get("user");
+  if (!user) {
+    return c.json(
+      {
+        errors: [
+          {
+            message: "Unauthorized",
+            code: "UNAUTHORIZED",
+          },
+        ],
+      },
+      401,
+    );
+  }
+  return next();
+});
