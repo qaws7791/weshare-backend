dd9770ca4c1f59ad0ea6abd059f5482013309421
qaws7791 <qaws7791@gmail.com>
2025-03-12 15:19:34 +0900

    스키마 생성 함수에 오류 응답 스키마 추가 및 반환 형식 개선

--- src/lib/create-schema.ts
+++ src/lib/create-schema.ts
@@ -1,11 +1,20 @@
+import { ErrorResponseSchema } from "@/shared/schema";
 import { ZodSchema } from "@/types/zod";
 import z from "zod";
 
-export const resourceContent = <T extends ZodSchema>(resourceSchema: T) => {
-  return z.object({
-    status: z.string(),
-    code: z.number(),
-    message: z.string(),
-    data: resourceSchema,
-  });
-};
+export const resourceContent = <T extends ZodSchema>(resourceSchema: T) => ({
+  "application/json": {
+    schema: z.object({
+      status: z.string(),
+      code: z.number(),
+      message: z.string(),
+      data: resourceSchema,
+    }),
+  },
+});
+
+export const errorContent = () => ({
+  "application/json": {
+    schema: ErrorResponseSchema,
+  },
+});
